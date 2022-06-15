23bf21cb0ef92b319f682224ecebcddf485fe396
sajad <18msajad.t@gmail.com>
2022-06-15 06:33:10 +0430

    mobile navbar

--- src/component/TopBar.jsx
+++ src/component/TopBar.jsx
@@ -4,9 +4,6 @@ import dot from "../Assets/Icon/Ellipse 1.png"
 import menu from "../Assets/Icon/menu.png"
 import close from "../Assets/Icon/close.png"
 
-
-
-
 // <<---header--->>
 
 export default function TopBar() {
@@ -16,24 +13,22 @@ export default function TopBar() {
     }
 
     return (
-        <div>
-            <div className={navbar ? " flex flex-col justify-end items-end text-right    " : " transition-all delay-300 w-full hidden  xl:flex xl:flex-row "}>
-                <img onClick={toggleNavbar} className={navbar ? " w-6 mr-6 mt-8   cursor-pointer" : "hidden"} src={close} alt="" />
-                <button className={navbar ? " relative btn-background items-center justify-center h-12 rounded-xl  w-40 flex p-2 flex-row m-7"
-                    : "  relative btn-background items-center justify-center h-12 rounded-xl xl:absolute w-40 flex p-2 flex-row m-7 "}>
-                    <span className=" text-cyan-50 self-center w-28 ">
-                        ورود / ثبت نام
-                    </span>
-                    <img className="w-4" src={user} alt="user" />
-                </button>
-                <ul className={navbar ? "flex mb-10 pb-4 p-2 mr-6  space-y-5 text-right  rounded-xl  xl:flex flex-col justify-center"
-                    : " xl:flex flex-row-reverse m-10 justify-center items-center w-full  space-x-2"}>
+        <div className="">
+            <div className= " w-{100px} justify-center relative  hidden  xl:flex xl:flex-row ">
+                <ul className={navbar ? " hidden"
+                    : " xl:flex flex-row-reverse m-10 justify-center items-center   space-x-2"}>
+                    <button className=" left-2  btn-background items-center justify-center h-12 rounded-xl absolute w-40 flex p-2 flex-row m-7 ">
+                        <span className=" text-cyan-50 self-center w-28 ">
+                            ورود / ثبت نام
+                        </span>
+                        <img className="w-4" src={user} alt="user" />
+                    </button>
 
                     <li className=" w-auto flex justify-center  items-center   xl:justify-between text-blue-500  " >
                         <span className=" xl:mx-10">
                             صفحه اصلی
                         </span>
-                        <img className={navbar ? " absolute ml-28  " : " absolute ml-14 mt-10 "} src={dot} alt="active" />
+                        <img className="" src={dot} alt="active" />
                     </li>
                     <li className="  w-auto">
                         <span className="mx-10">
@@ -57,15 +52,51 @@ export default function TopBar() {
                     </li>
                 </ul>
             </div>
-            <div className="flex flex-row  justify-end">
-                <button
-                    onClick={() => setNavbar(!navbar)}
-                    className={navbar ? " hidden" : " cursor-pointer transition-all delay-300 ease-in  text-white my-8 self-end mr-5  w-40  xl:hidden flex  flex-row justify-end"}
-                >
-                    <img className=" self-center" onClick={toggleNavbar} src={menu} alt="" />
-                </button>
+            <div className=" xl:hidden">
+            <ul className={navbar ? " absolute right-0  bg-white  flex flex-col justify-center items-center h w-full opacity-95   transition-all ease-in delay-500 space-x-2  "
+                                  : "absolute -right-full bg-white  flex flex-col justify-center items-center h w-full   transition-all ease-out delay-500 space-x-2  "}>
+                    <button className="  btn-background items-center justify-center h-12 rounded-xl  w-40 flex p-2 flex-row m-7 ">
+                        <span className=" text-cyan-50 self-center w-28 ">
+                            ورود / ثبت نام
+                        </span>
+                        <img className="w-4" src={user} alt="user" />
+                    </button>
+
+                    <li className=" w-auto flex justify-center p-2 m-7   items-center   xl:justify-between text-blue-500  " >
+                        <span className=" xl:mx-10">
+                            صفحه اصلی
+                        </span>
+                        <img className=" absolute ml-24" src={dot} alt="active" />
+                    </li>
+                    <li className="  w-auto p-2 m-7">
+                        <span className="mx-10">
+                            خدمات
+                        </span>
+                    </li>
+                    <li className=" w-auto p-2 m-7">
+                        <span className="mx-10">
+                            فروشگاه ها
+                        </span>
+                    </li>
+                    <li className=" w-auto p-2 m-7">
+                        <span className="mx-10">
+                            وبلاگ
+                        </span>
+                    </li>
+                    <li className=" w-auto  p-2 m-7">
+                        <span className="mx-10">
+                            ارتباط با ما
+                        </span>
+                    </li>
+                </ul>
             </div>
-         
+            <div className="flex flex-row  justify-end ">
+                <div onClick={toggleNavbar} className=" flex xl:hidden m-10 z-20 cursor-pointer ">
+                    {navbar ? <img src={close} /> : <img src={menu}   /> }
+
+                </div>
+            </div>
+
         </div>
     )
 }
